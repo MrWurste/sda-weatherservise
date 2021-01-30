633d561d5d1c3f03bb1e032f4b955bcf58f840ae
MrWurste <72695600+MrWurste@users.noreply.github.com>
2021-01-30 11:16:06 +0100

    Resolved NullPointerExeption when running test (edited repository mock)

--- src/main/java/com/sda/watherservice/backend/LocationRepositoryMock.java
+++ src/main/java/com/sda/watherservice/backend/LocationRepositoryMock.java
@@ -1,9 +1,10 @@
 package com.sda.watherservice.backend;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocationRepositoryMock implements LocationRepository{
-    List<Location> locations;
+    List<Location> locations = new ArrayList<>();
     @Override
     public Location saveNewLocation(Location location) {
         locations.add(location);
