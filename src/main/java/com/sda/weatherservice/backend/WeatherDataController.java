7bd67f81ceaa24c24f6eedf5aaeb69fe6c98a065
MrWurste <72695600+MrWurste@users.noreply.github.com>
2021-01-31 14:48:02 +0100

    Adding todos to make work easier

--- src/main/java/com/sda/weatherservice/backend/WeatherDataController.java
+++ src/main/java/com/sda/weatherservice/backend/WeatherDataController.java
@@ -4,6 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class WeatherDataController {
+    //todo split downloanding data for every location and one location
+    //todo optional - getRidOfDownlanding data for every location - with big Location database may it cause problems
+    //todo getOnly chosen weather datas from database, not every existing record
+    //todo downloanding data for one location
+    //todo add same stuff to future weather
+    //todo add to WeatherData entity date and time of forecast
+    //todo implement to WeatherData entity OneToMany relation (1 city - many weather datas)
 
     private final WeatherDataMapper weatherDataMapper = new WeatherDataMapper();
     private final WeatherDataRepository weatherDataRepository = new WeatherDataRepositoryImpl();
